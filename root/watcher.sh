accf4aaa42be675046391411604fb7529857f10f
Ricky Cook <mail@thatpanda.com>
2017-04-19 16:16:07 +1000

    Add comments to watcher

--- root/watcher.sh
+++ root/watcher.sh
@@ -22,17 +22,39 @@ END
 function regen_config() {
   cont_id="$1"
   host="$2"
+
+  # Public port for contanier port 80
   port=$(docker port $cont_id 80 2>/dev/null | awk -F ':' '{print $2}')
+
+  # If container doesn't have a port 80, we're done
   [ -n "$port" ] || return
+
   cont_name=$(docker inspect --format '{{.Name}}' $cont_id)
-  cont_name=${cont_name:1}
+  cont_name=${cont_name:1}  # Strip the / off the front
+
+  # Server prefix is everything before the first _
+  # Example:
+  #   > docker-compose -p 'test.lse' up
+  #   > docker ps --format '{{.Names}}'
+  #       test.lse_api_1
+  #       test.lse_app_1
+  #       ...
+  #   This will result in a server prefix of `test.lse` (thus
+  #   test.lse.bluebike.hosting)
   server_prefix=$(echo "$cont_name" | awk -F '_' '{ print $1 }')
+
+  # Do it!
   write_config $server_prefix $port $host
 }
 
 function regen_configs {
+  # Remove all old configs
   rm -f "$CONF_PATH"/*.docker.conf
+
+  # Default router IP is the Docker host
   host=$(route | awk '$1 ~ /default/ {print $2}')
+
+  # Try ang generate config for every container
   for cont_id in $(docker ps --all --format '{{.ID}}'); do
     regen_config "$cont_id" "$host"
   done
@@ -44,6 +66,7 @@ function process() {
 }
 
 function process_line {
+  # Limit the lines that we process to container create and destroy events
   cont_id=$(echo "$1" | awk '$2 ~ /container/ && $3 ~ /destroy|create/ { print $4 }')
   [ -n "$cont_id" ] || return
   process
